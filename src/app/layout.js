1e552af104c242eeb69dd1321afb52be37ddeb59
Ruby Lu <rubylu12345@gmail.com>
2025-04-29 23:35:13 -0400

    fix mobile layout (?)

--- src/app/layout.js
+++ src/app/layout.js
@@ -189,8 +189,8 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet" />
       </head>
-      <body className="bg-[var(--background)] text-[var(--primary)] transition-all" style={{ height: '100dvh', width: '100vw', overflow: 'hidden' }} ref={containerRef}>
-        <div className="flex h-full" style={{ height: '100dvh' }}>
+      <body className="bg-[var(--background)] text-[var(--primary)] transition-all" style={{ height: '100dvh', width: '100dvw', overflow: 'hidden' }} ref={containerRef}>
+        <div className="flex" style={{ height: '100dvh' }}>
           {isMobile && pathname !== '/' && (
             <div className="fixed top-4 right-4 z-[1000]" ref={menuRef}>
               <button onClick={toggleMobileMenu} className="transition-all p-2 border border-primary dark:border-darkSecondary bg-background dark:bg-darkBackground2" aria-label="Menu">
