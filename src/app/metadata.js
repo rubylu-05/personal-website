32810d20f280eaad63794e33914050c1c303ff86
Ruby Lu <rubylu12345@gmail.com>
2025-05-06 22:34:28 -0400

    metadata

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,7 +1,30 @@
 export const metadata = {
-    title: 'Ruby Lu',
-    icons: {
-      icon: '/favicon.png'
-    }
-  };
-  
+  title: 'Ruby Lu',
+  description: 'Showcasing my professional history, passion projects, and personal interests.',
+  metadataBase: new URL('https://www.rubylu.dev'),
+  icons: {
+    icon: '/favicon.ico'
+  },
+  openGraph: {
+    title: 'Ruby Lu | CS @ UWaterloo',
+    description: 'Showcasing my professional history, passion projects, and personal interests.',
+    url: 'https://www.rubylu.dev/',
+    siteName: 'Ruby Lu',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.rubylu.dev/images/preview.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Ruby Lu Portfolio Preview',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ruby Lu | CS @ UWaterloo',
+    description: 'Showcasing my professional history, passion projects, and personal interests.',
+    images: ['https://www.rubylu.dev/images/preview.jpeg'],
+  }
+};
